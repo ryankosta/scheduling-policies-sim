c7ff370a338a20a68cbe72d2078e03d81f6e6c7a
Ryan Kosta <rkosta@ucsd.edu>
2022-09-09 16:39:48 +0000

    Create presentation dir

--- presentation/000_dr_simplewarn-09-09-22/graph.py
+++ presentation/000_dr_simplewarn-09-09-22/graph.py
@@ -4,18 +4,19 @@ import sys
 import matplotlib.pyplot as plt
 
 lat_fields = ["99.9% Tail Latency","99% Tail Latency","95% Tail Latency"]
+files = ["dr-nowarn-varyload-manycores-fromdr.out",  "dr-warn-varyload-manycores-7ms.out"]
 fields = lat_fields.copy()
 fields.append("Cores")
-df1 = pd.read_csv(sys.argv[1])[fields]
-df2 = pd.read_csv(sys.argv[2])[fields]
-df1_rename = dict()
-df2_rename = dict()
+nowarn_df = pd.read_csv(files[0])[fields]
+warn_df = pd.read_csv(files[1])[fields]
+nowarn_df_rename = dict()
+warn_df_rename = dict()
 for field in lat_fields:
-    df1_rename[field] = "No Warn: " + field
-    df2_rename[field] = "Warn: " + field
-df1 = df1.rename(columns=df1_rename)
-df2 = df2.rename(columns=df2_rename)
-merge_df = pd.merge(df1,df2)
+    nowarn_df_rename[field] = "No Warn: " + field
+    warn_df_rename[field] = "Warn: " + field
+nowarn_df = nowarn_df.rename(columns=nowarn_df_rename)
+warn_df = warn_df.rename(columns=warn_df_rename)
+merge_df = pd.merge(nowarn_df,warn_df)
 for field in lat_fields:
     graph_df = merge_df[["Cores","Warn: " + field, "No Warn: " + field]]
     graph_df = graph_df[graph_df["Cores"] != 16]
